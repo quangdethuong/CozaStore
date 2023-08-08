8980800f8154a92ddec3f37f72c5a9c1ea3cc76a
Le Xuan Phuoc <stevenmarks147@gmail.com>
2023-08-09 00:10:37 +0700

    Add pagging BE

--- src/main/java/com/example/CozaStore/controller/BlogController.java
+++ src/main/java/com/example/CozaStore/controller/BlogController.java
@@ -1,6 +1,7 @@
 package com.example.CozaStore.controller;
 
 import com.example.CozaStore.payload.BlogMonthDTO;
+import com.example.CozaStore.payload.request.PagingRequest;
 import com.example.CozaStore.payload.response.BaseResponse;
 import com.example.CozaStore.service.imp.BlogServiceImp;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,16 +13,33 @@ import java.util.HashMap;
 
 @RestController
 @RequestMapping("/blog")
+@CrossOrigin(origins = "*")
 public class BlogController {
 
     @Autowired
     private BlogServiceImp blogServiceImp;
 
-    @GetMapping("")
-    public ResponseEntity<?> getAllAndPagingBlog(@RequestParam HashMap<String,String> hashMap){
-        int page = Integer.parseInt(hashMap.getOrDefault("page","0"));
-        int size = Integer.parseInt(hashMap.getOrDefault("size","3"));
+//    @GetMapping("")
+//    public ResponseEntity<?> getAllAndPagingBlog(@RequestParam HashMap<String,String> hashMap){
+//        int page = Integer.parseInt(hashMap.getOrDefault("page","0"));
+//        int size = Integer.parseInt(hashMap.getOrDefault("size","3"));
+//
+//        return new ResponseEntity<>(blogServiceImp.getAllBlog(page, size), HttpStatus.OK);
+//    }
+    @PostMapping("")
+    public ResponseEntity<?> getAllAndPagingBlog(
+            @RequestBody PagingRequest pagingReq ){
+//        int page = Integer.parseInt(hashMap.getOrDefault("page","0"));
+//        int size = Integer.parseInt(hashMap.getOrDefault("size","3"));
+        // default value
+        int page = 0;
+        int size = 3;
 
+        // if in request body has json parameter --> PagingRequest auto map key value <> use default value
+        if(pagingReq != null){
+            page = pagingReq.getPage();
+            size = pagingReq.getSize();
+        }
         return new ResponseEntity<>(blogServiceImp.getAllBlog(page, size), HttpStatus.OK);
     }
 
@@ -35,7 +53,6 @@ public class BlogController {
     @GetMapping("/number")
     public ResponseEntity<?> getNumberOfBlogByMonth(){
         BlogMonthDTO dto = blogServiceImp.getNumberBlogByMonth();
-
         return new ResponseEntity<>(dto,HttpStatus.OK);
     }
 
---
+++ src/main/java/com/example/CozaStore/payload/request/PagingRequest.java
@@ -0,0 +1,27 @@
+package com.example.CozaStore.payload.request;
+
+public class PagingRequest {
+    int page;
+    int size;
+
+    public PagingRequest(int page, int size) {
+        this.page = page;
+        this.size = size;
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+}
